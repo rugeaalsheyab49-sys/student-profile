9af290e3957fdded68dfd14b9dffa9b07d308538
Rugea Ahmad Alsheyab <rugeaalsheyab49@gmail.com>
2026-09-14 16:56:06 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Ruqea Ahmad Alsheyab"
+
+major = "Electronic Business Technology"
+
+university = "Al-Balqa Applied University"
+
+print("Student Profile")
+
+print("---")
+
+print("Name:", name)
+
+print("Major:", major)
+
+print("University:", university)
